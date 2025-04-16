2797314aa8cd967dee4d00ed6c114b9d80ea5572
XiaoA_A <22015564@qq.com>
2025-04-16 14:01:47 +0800

    修改资源接口描述

--- js/config.js
+++ js/config.js
@@ -16,100 +16,52 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    heimuer: {
-        api: 'https://json.heimuer.xyz',
-        name: '黑木耳',
-        detail: 'https://heimuer.tv'
-    },
-    ffzy: {
-        api: 'http://ffzy5.tv',
-        name: '非凡影视',
-        detail: 'http://ffzy5.tv'
-    },
-    tyyszy: {
-        api: 'https://tyyszy.com',
-        name: '天涯资源',
-    },
-    ckzy: {
-        api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
-    },
-    zy360: {
-        api: 'https://360zy.com',
-        name: '360资源',
+    dyttzy: {
+        api: 'https://caiji.dyttzyapi.com',
+        name: '[无广告]电影天堂',
     },
     wolong: {
         api: 'https://wolongzyw.com',
-        name: '卧龙资源',
-    },
-    cjhw: {
-        api: 'https://cjhwba.com',
-        name: '新华为',
-    },
-    hwba: {
-        api: 'https://cjwba.com',
-        name: '华为吧资源',
-    },
-    jisu: {
-        api: 'https://jszyapi.com',
-        name: '极速资源',
-        detail: 'https://jszyapi.com'
-    },
-    dbzy: {
-        api: 'https://dbzy.com',
-        name: '豆瓣资源',
-    },
-    bfzy: {
-        api: 'https://bfzyapi.com',
-        name: '暴风资源',
-    },
-    mozhua: {
-        api: 'https://mozhuazy.com',
-        name: '魔爪资源',
-    },
-    mdzy: {
-        api: 'https://www.mdzyapi.com',
-        name: '魔都资源',
+        name: '[去广]卧龙资源',
     },
     ruyi: {
         api: 'https://cj.rycjapi.com',
-        name: '如意资源',
+        name: '[去广]如意资源',
     },
     
     jkun: {
         api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
+        name: '[18+]jkun资源',
         adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
-        name: '百万资源',
+        name: '[18+]百万资源',
         adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
-        name: 'souav资源',
+        name: '[18+]souav资源',
         adult: true
     },
     siwa: {
         api: 'https://siwazyw.tv',
-        name: '丝袜资源',
+        name: '[18+]丝袜资源',
         adult: true
     },
     r155: {
         api: 'https://155api.com',
-        name: '155资源',
+        name: '[18+]155资源',
         adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
-        name: 'lsb资源',
+        name: '[18+]lsb资源',
         adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
-        name: '黄色仓库',
+        name: '[18+]黄色仓库',
         adult: true,
         detail: 'https://hsckzy.vip' // 添加detail URL以便特殊处理
     }
